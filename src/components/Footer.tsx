4eee2ca27f6a5e4f9f6981d849ce00cb163fc449
Luke Passey <ljpassey@gmail.com>
2024-03-23 15:14:46 -0600

    Update footer styling

--- src/components/Footer.tsx
+++ src/components/Footer.tsx
@@ -6,20 +6,20 @@ import { NavLink } from '@/components/NavLink'
 
 export function Footer() {
   return (
-    <footer className="bg-slate-50  ">
+    <footer className=" ">
       <Container>
-        <div className="flex w-full items-center justify-between py-2">
-          <Logo className="mx-auto h-10 w-auto" />
-          <nav className=" text-sm" aria-label="quick links">
-            <div className="  ">
+        <div className="flex flex-row  items-center justify-between py-2 ">
+          <Logo className="px-10" />
+          <nav className="  " aria-label="quick links">
+            <div className="text-sm px-1">
               <NavLink href="#features">Features</NavLink>
               <NavLink href="#testimonials">Testimonials</NavLink>
               <NavLink href="#pricing">Pricing</NavLink>
             </div>
           </nav>
         </div>
-        <div className="flex justify-center items-center border-t border-slate-400/10 py-10 sm:flex-row-reverse sm:justify-between">
-          <p className="mt-6  text-sm text-slate-500 sm:mt-0">
+        <div className="flex  items-center justify-center border-t border-slate-400/10 py-2  ">
+          <p className="  text-sm text-slate-500 sm:mt-0">
             Copyright &copy; {new Date().getFullYear()} Limelight Prep. All
             rights reserved.
           </p>
